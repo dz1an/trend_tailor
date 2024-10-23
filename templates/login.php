d839c07caa9a68a40ee884b7d76a7411b65bb1ca
dz1an <dzian2k17@gmail.com>
2024-10-23 15:12:07 +0800

    color

--- templates/login.php
+++ templates/login.php
@@ -40,8 +40,8 @@
 <!-- Designer Modal -->
 <div id="designerModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative w-full max-w-md max-h-full">
-        <div class="relative bg-white bg-opacity-95 rounded-lg shadow-lg">
-            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center" data-modal-hide="designerModal">
+        <div class="relative bg-gray-200 rounded-lg shadow-lg"> 
+            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-300 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center" data-modal-hide="designerModal">
                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                 </svg>
@@ -58,7 +58,7 @@
                     <div class="flex justify-between">
                         <a href="#" class="text-sm text-blue-700 hover:underline">Forgot Password?</a>
                     </div>
-                    <button type="submit" class="w-full text-white bg-black hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-400 font-medium rounded-full text-sm px-5 py-2.5 text-center">Sign In</button>
+                    <button type="submit" class="w-full text-white bg-gray-800 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-gray-400 font-medium rounded-full text-sm px-5 py-2.5 text-center">Sign In</button>
                     <div class="text-sm font-medium text-gray-500">
                         Not registered? <a href="#" class="text-blue-700 hover:underline">Sign Up</a>
                     </div>
@@ -71,8 +71,8 @@
 <!-- Client Modal -->
 <div id="clientModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative w-full max-w-md max-h-full">
-        <div class="relative bg-white bg-opacity-95 rounded-lg shadow-lg">
-            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center" data-modal-hide="clientModal">
+        <div class="relative bg-gray-200 rounded-lg shadow-lg"> 
+            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-300 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center" data-modal-hide="clientModal">
                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                 </svg>
@@ -89,7 +89,7 @@
                     <div class="flex justify-between">
                         <a href="#" class="text-sm text-blue-700 hover:underline">Forgot Password?</a>
                     </div>
-                    <button type="submit" class="w-full text-white bg-black hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-400 font-medium rounded-full text-sm px-5 py-2.5 text-center">Sign In</button>
+                    <button type="submit" class="w-full text-white bg-gray-800 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-gray-400 font-medium rounded-full text-sm px-5 py-2.5 text-center">Sign In</button>
                     <div class="text-sm font-medium text-gray-500">
                         Not registered? <a href="#" class="text-blue-700 hover:underline">Sign Up</a>
                     </div>
@@ -98,6 +98,7 @@
         </div>
     </div>
 </div>
+
 
 
     <!-- Footer -->
